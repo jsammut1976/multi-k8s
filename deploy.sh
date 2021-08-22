470964f60a304e151dd3e79fbcabb4e3216ab108
John Sammut <sammutjohn@gmail.com>
2021-08-22 14:27:26 +0200

    adding deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t jsammut/multi-client:latest -t multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jsammut/multi-server:latest -t multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jsammut/multi-worker:latest -t multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push jsammut/multi-client:latest
+docker push jsammut/multi-server:latest
+docker push jsammut/multi-worker:latest
+docker push jsammut/multi-client:$SHA
+docker push jsammut/multi-server:$SHA
+docker push jsammut/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployment/server-deployment server=jsammut/multi-server:$SHA
+kubectl set image deployment/client-deploymnet client=jsammut/multi-client:$SHA
+kubectl set image deploymnet/worker-deployment worker=jsammut/multi-worker:$SHA
