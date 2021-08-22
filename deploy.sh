e77c96c78c8e5c3440f5f166adbd797d0a9baef2
John Sammut <sammutjohn@gmail.com>
2021-08-22 15:37:57 +0200

    fixing docker build statements in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t jsammut/multi-client:latest -t multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t jsammut/multi-server:latest -t multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t jsammut/multi-worker:latest -t multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t jsammut/multi-client:latest -t jsammut/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jsammut/multi-server:latest -t jsammut/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jsammut/multi-worker:latest -t jsammut/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push jsammut/multi-client:latest
 docker push jsammut/multi-server:latest
 docker push jsammut/multi-worker:latest
@@ -8,6 +8,6 @@ docker push jsammut/multi-client:$SHA
 docker push jsammut/multi-server:$SHA
 docker push jsammut/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployment/server-deployment server=jsammut/multi-server:$SHA
-kubectl set image deployment/client-deployment client=jsammut/multi-client:$SHA
-kubectl set image deployment/worker-deployment worker=jsammut/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=jsammut/multi-server:$SHA
+kubectl set image deployments/client-deployment client=jsammut/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=jsammut/multi-worker:$SHA
